d5ed8adc4fa22c92af79a752977ad499765f4e80
Silvan Adrian <hallo@silvanadrian.ch>
2019-03-24 00:37:02 +0100

    more

--- classification.py
+++ classification.py
@@ -1,1 +1,10 @@
-# Classification Component
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
+from nltk.corpus import stopwords
+
+questions = pd.read_csv("data/train_dataset.csv", header=None, encoding="iso-8859-1", sep=";")
+
+
+
