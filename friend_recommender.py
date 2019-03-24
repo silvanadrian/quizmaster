65bee44615334e2b01f835c5e7a05c3ac7ae74dd
Silvan Adrian <hallo@silvanadrian.ch>
2019-03-24 20:31:53 +0100

    topic base friend recommender

--- friend_recommender.py
+++ friend_recommender.py
@@ -1,7 +1,44 @@
+import pandas as pd
+from scipy.stats import pearsonr
+
+
+def takeCorrelation(elem):
+  return elem[1]
 
 
 def main():
-  input("Which friend recommending method? topic? features?")
-  
+
+  questions = pd.read_csv("data/classified.csv",
+                          encoding="utf-8", sep=",")
+
+  friends = []
+  type = input("Which friend recommending method? topic? features?")
+  if type == "topic":
+    print("Topic based recommendation:")
+    user_id = input("Please input your user id:")
+    cutoff_k = input("Cutoff k:")
+
+    user_features = questions[questions['id'] == int(user_id)].groupby(['topic'])['opinion'].mean()
+    questions = questions.groupby(['id'])
+
+    for n, g in questions:
+      if n == int(user_id):
+        continue
+      # when classification not works properly then some users have only 4 topics
+      if len(g.groupby(['topic'])['opinion'].mean()) < 5:
+        continue
+      corr, p_value = pearsonr(user_features, g.groupby(['topic'])['opinion'].mean())
+      friends.append((n, corr))
+
+    friends.sort(key=takeCorrelation,reverse=True)
+    for friend in friends[:int(cutoff_k)]:
+      print(friend)
+
+  else:
+    print("Feature based recommendation")
+    
+
+
+
 
 if __name__ == "__main__": main()
